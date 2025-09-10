703e4b867eda7fcfc1d2299ac6ac5f1983c25a33
Thembinkosid <jucbottle@gmail.com>
2025-09-10 10:27:22 +0200

    Removed unwanted files

--- runserver_plus.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Django development server (HTTP)...
-echo.
-echo Access the site at: http://127.0.0.1:8000
-echo.
-set DEBUG=true
-python manage.py runserver 127.0.0.1:8000
-pause
